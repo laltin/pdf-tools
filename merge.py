0147d1632cdee0b9ad654d7d133283458ccfc65a
Lütfi Altın <lutfi.altin@gmail.com>
2016-03-30 21:40:33 +0300

    pdf merger added

---
+++ merge.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import pyPdf
+import sys
+
+
+def merge (input_files, output_file):
+    """
+    merges given input pdf files into single pdf file
+    """
+
+    output = pyPdf.PdfFileWriter()
+    
+    for input_file in input_files:
+        input = pyPdf.PdfFileReader( open(input_file, "rb") )
+        
+        for i in range(input.numPages):
+            output.addPage(input.getPage(i))
+
+    with open(output_file, "wb") as outputStream:
+        output.write(outputStream)
+
+    return True
+
+
+def main (args):
+    # at least 3 arguments are needed: program name itself, input, output
+    if len(args) < 3:
+        print "Usage: %s input1... output" % args[0]
+        return
+    
+    input_files = args[1:-1]
+    output_file = args[-1]
+    
+    merge(input_files, output_file)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
